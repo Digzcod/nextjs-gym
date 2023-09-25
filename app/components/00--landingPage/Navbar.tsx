2f15e9d69b616595664ac44d0a7534dbfe146ecb
Digzcod <stephenjaramillo5@gmail.com>
2023-09-25 10:25:47 +0800

    new commits

--- app/components/00--landingPage/Navbar.tsx
+++ app/components/00--landingPage/Navbar.tsx
@@ -40,11 +40,11 @@ export default function Navbar() {
     <>
       <nav className="navbar shadow-md h-[5rem font-medium flex justify-center space-x-3 py-4">
         <div className="flex items-baseline">
-          <AiTwotoneHome className="text-gray-600  text-2xl font-semibold mr-[-.5rem]" />
+          <AiTwotoneHome className="text-gray-500  text-2xl font-semibold mr-[-.5rem]" />
           <button className="mx-3 text-lg font-medium my-1">Home</button>
         </div>
         <div className="flex items-baseline">
-          <BsFillLayersFill className="text-gray-600 text-2xl font-semibold mr-[-.5rem]" />
+          <BsFillLayersFill className="text-gray-500 text-2xl font-semibold mr-[-.5rem]" />
           <button className="mx-3 text-lg font-medium my-1">Projects</button>
         </div>
         <div className="flex items-baseline">
