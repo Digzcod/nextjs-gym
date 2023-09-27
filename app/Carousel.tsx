a07e6e0e4a4465533ad56b55c53c009dba1d8293
Digzcod <stephenjaramillo5@gmail.com>
2023-09-27 22:17:54 +0800

    new commits

--- app/Carousel.tsx
+++ app/Carousel.tsx
@@ -1,12 +1,12 @@
 
 import React from 'react'
 import Image from 'next/image'
-import jsLogo from "../public/JavaScript-logo.png"
+import jsLogo from "../public/logo-javascript.svg"
 import tsLogo from "../public/typescript-2.svg"
 import reactLogo from "../public/React-icon.svg.png"
 import nextsjsLogo from "../public/nextjs.png"
-import muiLogo from "../public/mui.png"
-import tailwindLogo from "../public/tailwindcss-ar21.png"
+import muiLogo from "../public/material-ui-1.svg"
+import tailwindLogo from "../public/tailwind-css-1.svg"
 import Slider from "react-slick";
 
 
@@ -15,22 +15,22 @@ export default function Carousel() {
   return (
       
       <section className='carousel rounded-box sm:space-x-8 flex justify-center space-x-4'>
-        <div className='carousel-item w-[50px] h-[50px] sm:w-[70px] sm:h-[70px]'>
+        <div className='carousel-item w-[50px] h-[50px] sm:w-[60px] sm:h-[60px]'>
             <Image src={jsLogo} alt='js' className='w-full h-full' placeholder='empty' />
         </div>
-        <div className='carousel-item w-[50px] h-[50px] sm:w-[70px] sm:h-[70px]'>
+        <div className='carousel-item w-[50px] h-[50px] sm:w-[60px] sm:h-[60px]'>
             <Image src={tsLogo} alt='js' className='w-full h-full' placeholder='empty'/>
         </div>
-        <div className='carousel-item w-[55px] h-[50px] sm:w-[75px] sm:h-[70px]'>
+        <div className='carousel-item w-[55px] h-[50px] sm:w-[70px] sm:h-[70px]'>
             <Image src={reactLogo} alt='js' className='w-full h-full' placeholder='empty'/>
         </div>
-        <div className='carousel-item w-[70px] h-[50px] sm:w-[90px] sm:h-[70px]'>
+        <div className='carousel-item w-[50px] h-[50px] sm:w-[70px] sm:h-[70px]'>
             <Image src={nextsjsLogo} alt='js' className='w-full h-full' placeholder='empty'/>
         </div>
-        <div className='carousel-item w-[70px] h-[50px] sm:w-[80px] sm:h-[70px]'>
+        <div className='carousel-item w-[50px] h-[50px] sm:w-[70px] sm:h-[70px]'>
             <Image src={muiLogo} alt='js' className='w-full h-full' placeholder='empty'/>
         </div>
-        <div className='carousel-item w-[90px] h-[50px]  sm:w-[110px] sm:h-[70px]'>
+        <div className='carousel-item w-[50px] h-[50px]  sm:w-[70px] sm:h-[70px]'>
             <Image src={tailwindLogo} alt='js' className='w-full h-full' placeholder='empty'/>
         </div>
     </section>
