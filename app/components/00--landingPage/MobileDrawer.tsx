7a96efa9ee87beefeffcd0db26f38ab51bf0215d
Digzcod <stephenjaramillo5@gmail.com>
2023-11-07 18:38:16 +0800

    resize the icon

--- app/components/00--landingPage/MobileDrawer.tsx
+++ app/components/00--landingPage/MobileDrawer.tsx
@@ -55,7 +55,7 @@ export default function MobileDrawer({
       >
         <div className="flex items-center justify-end w-screen px-[1rem] py-[2rem] border-b-2 shadow-lg">
           <button onClick={() => setOpen(!open)}>
-            <VscChromeClose className="text-[3rem] font-[900]/"/>
+            <VscChromeClose className="text-[2.5rem] font-[900]/"/>
           </button>
         </div>
 
